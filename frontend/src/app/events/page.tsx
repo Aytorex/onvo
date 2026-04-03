4b1ab9f2a33237974d96801fe783e6a912995650
Joshua Kaczmarek <joshua.kaczmarek@gmail.com>
2026-04-03 23:00:00 +0200

    refactor: remove ballot pages and rebrand to Onvo

--- frontend/src/app/events/page.tsx
+++
@@ -1,5 +0,0 @@
-import { EventsSection } from '@/app/events/events-section';
-
-export default function Events() {
-  return <EventsSection />;
-}
--- frontend/src/app/proposals/page.tsx
+++
@@ -1,9 +0,0 @@
-import { ProposalsList } from '@/components/shared/proposals-list';
-
-export default function Proposals() {
-  return (
-    <>
-      <ProposalsList displayVotes={false} />
-    </>
-  );
-}
